988b28d686e07586683ee32fe122f446053dda12
Stuart Doney <stu@Stuarts-MacBook-Air.local>
2018-09-24 14:10:39 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,12 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Connect to server');
 
-    socket.emit('createMessage', {
-        from: 'Stuart',
-        text: 'Yep thats works for me'
-    })
 });
 
 socket.on('disconnect', function(){
     console.log('Disconnect from server');
 });
 
-socket.on('newMessage', function(message){
-    console.log('newMessage')
-})
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+  });

a4a574781b1660edd471ceae30311138b45cc7b5
Stuart Doney <stu@stuarts-air.home>
2018-09-27 00:19:43 +0100

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -30,12 +30,13 @@ socket.on('newLocationMessage', function(message){
 
   jQuery('#message-form').on('submit', function(e){
       e.preventDefault();
+      var messageTextbox = jQuery('[name=message]')
 
       socket.emit('createMessage', {
           from: 'User',
-          text: jQuery('[name=message]').val()
+          text: messageTextbox.val()
       },function(){
-
+            messageTextbox.val('')
       })
   });
 
@@ -46,12 +47,16 @@ socket.on('newLocationMessage', function(message){
           return alert('Geolocation not supported by your browser.');
       }
 
+      locationButton.attr('disabled', 'disabled').text('Sending location...');
+
       navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         })
       }, function(){
-          alert('Unable to fetch location.')
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location.')
       })
   })
--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+
 io.on('connection', (socket) => {
     console.log('New user Connected');
 
@@ -24,7 +25,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
